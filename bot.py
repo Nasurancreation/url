1bc0f7a6c34da8c5300b841c76a9f85d8d08663a
Nasurancreation <88917315+Nasurancreation@users.noreply.github.com>
2022-03-21 20:42:08 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,4 @@
+
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # (c) Shrimadhav U K | Modifieded By : @DC4_WARRIOR
@@ -11,7 +12,7 @@ if __name__ == "__main__" :
     if not os.path.isdir(Config.DOWNLOAD_LOCATION):
         os.makedirs(Config.DOWNLOAD_LOCATION)
     plugins = dict(root="plugins")
-    Warrior = Clinton("@UPLOADER_X_BOT",
+    Warrior = Clinton("@Nasuranurluploader_bot",
     bot_token=Config.BOT_TOKEN,
     api_id=Config.API_ID,
     api_hash=Config.API_HASH,
